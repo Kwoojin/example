567ed997cc8a74d68ed2778600eff71fffcc1eaa
WoojinKim <dnwls488@naver.com>
2022-06-29 19:10:26 +0900

    강연 등록 api 추가

--- src/main/java/studio/example/reservation/api/ReservationFrontController.java
+++ src/main/java/studio/example/reservation/api/ReservationFrontController.java
@@ -1,19 +1,41 @@
 package studio.example.reservation.api;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import studio.example.lecture.api.LectureBackOfficeController;
+import studio.example.lecture.domain.Lecture;
+import studio.example.reservation.domain.Reservation;
+import studio.example.reservation.service.ReservationService;
+
+import static studio.example.reservation.api.ReservationFrontController.ReservationDto.*;
 
 @RestController
 @RequestMapping("/front/reservations")
 @RequiredArgsConstructor
 public class ReservationFrontController {
 
-//    @PostMapping("/{lectureId}")
-//    public ResponseEntity<?> addReservation(@RequestBody String id) {
-//
-//    }
+    private final ReservationService reservationService;
+
+    @PostMapping("/{lectureId}")
+    public ResponseEntity<?> addReservation(@RequestBody String empNo, @PathVariable Long lectureId) {
+        long id = reservationService.addReservation(empNo, lectureId);
+        return new ResponseEntity<>(createLectureDto(id, empNo, lectureId), HttpStatus.CREATED);
+    }
+
+    @Getter
+    static class ReservationDto {
+        private Long id;
+        private String empNo;
+        private Long lectureId;
+        public static ReservationDto createLectureDto(Long id, String empNo, Long lectureId) {
+            ReservationDto dto = new ReservationDto();
+            dto.id = id;
+            dto.empNo = empNo;
+            dto.lectureId = lectureId;
+            return dto;
+        }
+    }
 }
