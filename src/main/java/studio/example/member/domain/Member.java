12f5bd8582e3b126be9d94132a257b2982d3dee7
WoojinKim <dnwls488@naver.com>
2022-06-29 19:11:28 +0900

    hash equals 추가

--- src/main/java/studio/example/member/domain/Member.java
+++ src/main/java/studio/example/member/domain/Member.java
@@ -1,6 +1,7 @@
 package studio.example.member.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import studio.example.model.BaseTimeEntity;
@@ -9,6 +10,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -21,4 +23,25 @@ public class Member extends BaseTimeEntity {
     private Long id;
 
     private String name;
+
+    private String empNo;
+
+    @Builder
+    public Member(String name, String empNo) {
+        this.name = name;
+        this.empNo = empNo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(getId(), member.getId()) && Objects.equals(getEmpNo(), member.getEmpNo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getEmpNo());
+    }
 }
