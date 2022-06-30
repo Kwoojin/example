0904262c838b36e51a225c301b3f82d94be6f420
WoojinKim <dnwls488@naver.com>
2022-06-30 15:34:02 +0900

    실행 시 초기 데이터 수정

--- src/main/java/studio/example/InitData.java
+++ src/main/java/studio/example/InitData.java
@@ -4,10 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import studio.example.lecture.repo.LectureRepository;
+import studio.example.lecture.domain.Lecture;
+import studio.example.member.domain.Member;
+import studio.example.reservation.domain.Reservation;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static studio.example.lecture.domain.Lecture.*;
 
@@ -28,20 +34,40 @@ public class InitData {
     @RequiredArgsConstructor
     static class InitService {
 
-        private final LectureRepository lectureRepository;
+        @PersistenceContext EntityManager em;
 
         public void dbInit1() {
-            lectureRepository.save(createLecture(
-                "1", "1", "1", 100, LocalDateTime.now(), LocalDateTime.now(), "2"
-            ));
+            LocalDateTime now = LocalDateTime.now();
+            List<Lecture> lectures = new ArrayList<>();
+            for(int i=1; i<=20; i++){
+                Lecture lecture = createLecture(
+                        "title" + i, "place" + i, "lecturer" + i, 30 + i, now.minusDays(2).plusHours(i * 3), now.minusDays(2).plusHours(i * 3 + 1), "content" + i
+                );
+                lectures.add(lecture);
+                em.persist(lecture);
+            }
 
-            lectureRepository.save(createLecture(
-                    "2", "2", "2", 100, LocalDateTime.now().plusDays(7), LocalDateTime.now(), "2"
-            ));
+            List<Member> members = new ArrayList<>();
+            for(int i=1; i<=40; i++){
+                Member member = Member.builder().name("name" + i).empNo("X" + String.format("%04d", i)).build();
+                members.add(member);
+                em.persist(member);
+            }
 
-            lectureRepository.save(createLecture(
-                    "3", "3", "3", 100, LocalDateTime.now().minusDays(7), LocalDateTime.now(), "2"
-            ));
+            for(int i=0; i<15; i++) {
+                for(int j=i*2; j<i*2+2; j++){
+                    em.persist(Reservation.createReservation(members.get(j), lectures.get(i)));
+                }
+            }
+
+            for(int i=5; i<10; i++){
+                em.persist(Reservation.createReservation(members.get(i), lectures.get(0)));
+                em.persist(Reservation.createReservation(members.get(i+1), lectures.get(1)));
+            }
+
+            em.persist(Reservation.createReservation(members.get(10), lectures.get(2)));
+            em.persist(Reservation.createReservation(members.get(11), lectures.get(2)));
+            em.persist(Reservation.createReservation(members.get(12), lectures.get(2)));
         }
     }
 }
