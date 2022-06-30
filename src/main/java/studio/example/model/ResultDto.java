cde6657a9459836e7af1c59255932fdc263a0789
WoojinKim <dnwls488@naver.com>
2022-06-30 15:33:46 +0900

    기본 결과 타입 추가

--- src/main/java/studio/example/model/ResultDto.java
+++ src/main/java/studio/example/model/ResultDto.java
@@ -2,10 +2,16 @@ package studio.example.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
+import lombok.Getter;
 
-@Builder
+@Getter
 public class ResultDto<T> {
     @JsonIgnore
     private int count;
     private T content;
+
+    @Builder
+    public ResultDto(T content) {
+        this.content = content;
+    }
 }
